3f41fea0891afa1932b3357a31d0cc8ec08984d0
Adeola tawakaltu Adenike <tawakalituadeola68@gmail.com>
2022-08-08 13:17:59 +0100

    home page adjustment

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
         <span class="text-yellow-500 opacity-85"> Explore</span> Your World
       </h1>
     </div>
-    <div class="absolute right-36 bottom-4 ">
+    <div class="absolute right-36 top-16 ">
 <img src="../assets/images/beach-removebg-preview.png" alt="travel-palm-image" class=" h-96 w-96">
     </div>
     </div>
