19e13d2c396c7a132395fb1f73f21db310a11c4d
Adeola tawakaltu Adenike <tawakalituadeola68@gmail.com>
2022-09-07 13:02:50 +0100

    add more

--- src/components/OurTeam.vue
+++ src/components/OurTeam.vue
@@ -3,7 +3,7 @@
             <h1 class="capitalize font-bold text-3xl text-gray-700">Our team</h1>
             <div class="flex space-x-6 py-10">
                 <div v-for="i in team" :key="i">
-                    <img :src="i.picture" alt="" class="rounded-full w-20 h-20">
+                    <img :src="i.picture" alt="" class="rounded-full w-12 h-12">
                     <p class=" pt-4 text-lg font-bold">{{i.name}}</p>
                     <p class="text-center pt-2 text-lg">{{i.role}}</p>
                 </div>
